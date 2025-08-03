ed58a7e71158fcddfa7a2d6795fccea4b2d8101a
David <davidkhun2004@gmail.com>
2025-08-03 09:36:08 +0700

    Working on the navagation of the shopping project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 
+import Nav from "./components/Nav";
+
 function App() {
   return (
     <>
       <h1>React</h1>
+      <Nav />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import routes from "./router.jsx";
-import App from "./App.jsx";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter(routes);
