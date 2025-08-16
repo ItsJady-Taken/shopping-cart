887aa320fa0bc9ff25984651509da86cc0687981
David <davidkhun2004@gmail.com>
2025-08-16 16:01:12 +0700

    Working on purchase section

--- src/components/ViewPage.jsx
+++ src/components/ViewPage.jsx
@@ -1,18 +1,34 @@
 import "../styles/viewPage.css";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
 import { Rating, Box } from "@mui/material";
 
 function ViewPage() {
   const location = useLocation();
   const clothes = location.state;
+
+  const [quantity, setQuantity] = useState(1);
+
+  function onClickMinus() {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  }
+  function onClickPlus() {
+    setQuantity(quantity + 1);
+  }
+
   return (
     <section className="view-container">
       <div className="view-product">
         <div className="view-product-info">
+          {/* //title */}
           <h3>{clothes.title}</h3>
+          {/* //description */}
           <p className="view-desc">{clothes.description}</p>
+          {/* //rating */}
           <Box
             style={{ display: "flex", alignItems: "center", gap: "6px" }}
             sx={{ "& > legend": { mt: 2 } }}
@@ -25,10 +41,17 @@ function ViewPage() {
               ({clothes.rating.count})
             </p>
           </Box>
-          <p style={{ fontWeight: "bold", fontSize: "28px" }}>
+          {/* //price */}
+          <p style={{ fontWeight: "bold", fontSize: "28px", margin: "0px" }}>
             ${clothes.price}
           </p>
-
+          {/* //quantity */}
+          <QuantityButton
+            quantity={quantity}
+            onClickMinus={onClickMinus}
+            onClickPlus={onClickPlus}
+          />
+          {/* //buy / add to card */}
           <div style={{ display: "flex", gap: "10px" }}>
             <Button
               style={{ fontWeight: "bold", border: "1px solid black" }}
@@ -49,20 +72,43 @@ function ViewPage() {
           <img src={clothes.image} alt={clothes.title} />
         </div>
       </div>
-
-      <div className="view-product">
-        <div className="view-product-info">
-          <h3>{clothes.title}</h3>
-          <p>{clothes.price}</p>
-          <p>{clothes.rating.rate}</p>
-          <p>{clothes.rating.count}</p>
-        </div>
-
-        <div className="view-product-image">
-          <img src={clothes.image} alt={clothes.title} />
-        </div>
-      </div>
     </section>
+  );
+}
+
+function QuantityButton({ quantity, onClickMinus, onClickPlus }) {
+  return (
+    <div>
+      <p style={{ fontWeight: "bold", margin: "0px" }}>Quantity:</p>
+      <ButtonGroup
+        aria-label="Basic example"
+        style={{
+          border: "1px solid gray",
+        }}
+      >
+        <Button
+          style={{ fontWeight: "bold", fontSize: "18px" }}
+          onClick={onClickMinus}
+          variant="light"
+        >
+          -
+        </Button>
+        <Button
+          style={{ fontWeight: "bold", fontSize: "18px" }}
+          variant="light"
+          disabled
+        >
+          {quantity}
+        </Button>
+        <Button
+          style={{ fontWeight: "bold", fontSize: "18px" }}
+          onClick={onClickPlus}
+          variant="light"
+        >
+          +
+        </Button>
+      </ButtonGroup>
+    </div>
   );
 }
 
