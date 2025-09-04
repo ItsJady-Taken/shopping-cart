4e2c9baa253f20e86b208cb922d611670edcc6c0
David <davidkhun2004@gmail.com>
2025-09-04 20:48:52 +0700

    Working On Checkout section

--- src/components/CheckoutPage.jsx
+++ src/components/CheckoutPage.jsx
@@ -14,11 +14,28 @@ function CheckoutPage() {
           <div className="all-items">
             <div className="checkout-item">
               {checkoutList.map((clothes) => (
-                <div key={clothes[0].id} className="checkout-item-card">
+                <div
+                  key={clothes[0].id}
+                  className="checkout-item-card"
+                  style={{ position: "relative" }}
+                >
                   <img src={clothes[0].image} alt={clothes[0].title} />
+
                   <div>
-                    <h4>Hello{clothes[0].title}</h4>
+                    <h4>{clothes[0].title}</h4>
                     <p>{clothes[0].price}</p>
+                  </div>
+                  <div
+                    style={{
+                      position: "absolute",
+                      top: "10px",
+                      right: "10px",
+                      cursor: "pointer",
+                      fontWeight: "bold",
+                      fontSize: "20px",
+                    }}
+                  >
+                    X
                   </div>
                 </div>
               ))}
@@ -26,24 +43,24 @@ function CheckoutPage() {
           </div>
           <div className="sumary-container">
             <h3>Sumary</h3>
-            <div className="summary-total-info">
-              <p>Subtotal</p>
-              <p>{clotheslist[0].price}</p>
+            <div className="summary-total-info" style={{ margin: "0" }}>
+              <p style={{ fontWeight: "bold" }}>Subtotal</p>
+              <p style={{ fontWeight: "bold" }}>${clotheslist[0].price}</p>
             </div>
-            <div className="summary-total-info">
-              <p>Shipping</p>
-              <p>Free</p>
+            <div className="summary-total-info" style={{ margin: "0" }}>
+              <p style={{ fontWeight: "bold" }}>Shipping</p>
+              <p style={{ fontWeight: "bold" }}>Free</p>
             </div>
-            <div className="summary-total-info">
-              <p>Total</p>
-              <p>{clotheslist[0].price}</p>
+            <div className="summary-total-info" style={{ margin: "0" }}>
+              <p style={{ fontWeight: "bold" }}>Tax</p>
+              <p style={{ fontWeight: "bold" }}>$0.89</p>
             </div>
             <div
               className="summary-total-info"
               style={{ borderTop: "1px solid gray", padding: "10px 0px" }}
             >
-              <h5>Total</h5>
-              <h5>{clotheslist[0].price}</h5>
+              <h5 style={{ fontWeight: "bolder" }}>Total</h5>
+              <h5 style={{ fontWeight: "bolder" }}>${clotheslist[0].price}</h5>
             </div>
           </div>
         </div>
